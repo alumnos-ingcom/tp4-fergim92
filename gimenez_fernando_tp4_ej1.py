a713b19dfc5f29dfa9dcb34765636a56e7762eea
Fernando Marcos Gimenez <gimenezfernandomarcos@gmail.com>
2021-05-14 12:09:55 -0300

    Ejercicio 2 terminado.Preguntar si tenemos que pedir nosotros los numeros o solamente pasarlos como parametros

--- gimenez_fernando_tp4_ej1.py
+++ gimenez_fernando_tp4_ej1.py
@@ -9,6 +9,7 @@ class IngresoIncorrecto(Exception):
 
 
 def ingreso_entero(mensaje):
+    print('Funcion 1.Ingreso de numeros enteros')
     ingreso = input(mensaje + " #")
     try:
          entero = int(ingreso)
@@ -18,9 +19,11 @@ def ingreso_entero(mensaje):
 
 
 def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
+    print('\nFuncion 2.Ingresar un número entero y vuelva a solicitarlo en caso de ingresar un valor incorrecto')
     print(f'{mensaje}\nTiene {cantidad_reintentos} intentos')
     for i in range(cantidad_reintentos):
         ingreso = input(f'intento {i+1}: ')
+        
         try:
             entero = int(ingreso)
             return entero
@@ -33,27 +36,27 @@ def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
             
 
 def ingreso_entero_restringido(mensaje,valor_minimo=0, valor_maximo=10):
+    print('\nFuncion 3.Ingresar un número entre 2 rangos')
     print(f'{mensaje}{valor_minimo} y {valor_maximo}: ')
     ingreso = input()
     try:
         entero = int(ingreso)
         if (entero > valor_maximo) or (entero < valor_minimo):
             print('No esta en el rango solicitado')
+        else:
+            print('Tu numero esta en el rango')
     except ValueError as err:
         raise IngresoIncorrecto("No era un numero!") from err
+    
     return entero
 
     
 
 
 if __name__ == "__main__":
-    print('Funcion 1.Ingreso de numeros enteros')
-    ingreso_entero('Ingrese un numero entero')
     
-    print('\nFuncion 2.Ingresar un número entero y vuelva a solicitarlo en caso de ingresar un valor incorrecto')
+    ingreso_entero('Ingrese un numero entero') 
     ingreso_entero_reintento('Ingrese un numero entero', cantidad_reintentos=5)
- 
-    print('\nFuncion 3.Ingresar un número entre 2 rangos')
     ingreso_entero_restringido('Ingrese un numero entre ',valor_minimo=0, valor_maximo=10)
     
     
--- gimenez_fernando_tp4_ej2.py
+++ gimenez_fernando_tp4_ej2.py
@@ -1,3 +1,15 @@
-import gimenez_fernando_tp4_ej1 as soporte
-
-soporte.ingreso_entero("Ingrese un numero entero")
+def suma_lenta(numero, otro_numero):
+    limite = otro_numero
+    if otro_numero < 0:
+        limite *= -1
+        
+    for i in range(limite):
+        if otro_numero < 0:
+            resultado = numero - i-1
+            print(f'{numero-i} + (-1) = {resultado} ')
+        else:
+            resultado = numero + i+1
+            print(f'{numero+i} + 1 = {resultado} ')
+        
+if __name__ == "__main__":        
+    suma_lenta(-4, -10)
