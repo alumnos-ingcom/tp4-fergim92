aa598a105c946733b94e0c6b4c3a93a67885a173
Fernando Marcos Gimenez <gimenezfernandomarcos@gmail.com>
2021-05-15 21:51:14 -0300

    ejercicio 9 terminado.True no true

--- tp4_ej9.py
+++ tp4_ej9.py
@@ -5,17 +5,18 @@
 from tp4_ej1 import ingreso_entero 
 
 def es_primo(numero):
-    divisores = 0
+    divisores = 1
     for i in range(1, numero):
         if numero % i == 0:
             divisores += 1
     if divisores == 2:
-        return true         #########
+        return True
+    else:
+        return False
     
-
 def prueba():
     numero = ingreso_entero('Digite un numero para saber si es primo')
     print(es_primo(numero))
-
+    
 if __name__ == "__main__":
     prueba()
